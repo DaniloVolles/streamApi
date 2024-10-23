baebdb4be3f1dc23030c682bb3896496d6133bd2
Danilo Volles <danilo.volles@outlook.com>
2024-10-22 22:20:57 -0300

    SupplierExample

--- src/FunctionalInterface.java
+++ src/FunctionalInterface.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FunctionalInterface {
     public static void main(String[] args) {
@@ -35,3 +38,25 @@ class ConsumerExample {
     }
 }
 
+/**
+ * Operação que não aceita argumento e retorna um resultado do tipo T.
+ * É comumente usada para criar ou fornecer novos objetos de um determinado tipo.
+ */
+class SupplierExample {
+    public static void main(String[] args) {
+        Supplier<String> saudacao = () -> "Olá, seja bem vindo!";
+
+        List<String> listaSaudacoes = Stream.generate(saudacao)
+                .limit(5)
+                .toList();
+
+        listaSaudacoes.forEach(s -> System.out.println(s));
+
+        // forma mais sucinta
+        List<String> listaSaudacoesSimples = Stream.generate(() -> "Olá, seja bem vinda!")
+                .limit(5)
+                .toList();
+
+        listaSaudacoesSimples.forEach(s -> System.out.println(s));
+    }
+}
